375ea508f5658fb465fb03f58df517a235dcda5c
Hargun Sibal <58493658+hargun-27@users.noreply.github.com>
2022-09-17 19:15:57 -0400

    FINAL

--- pages/index.js
+++ pages/index.js
@@ -122,7 +122,7 @@ export default function Home() {
 
         <Typography variant="h2" className={styles.title} sx={{ marginTop: 3 }}>
         Maintain a spotless online persona - anywhere, anytime.
-        </Typography>
+        </Typography><br></br>
         <Typography variant="h5" className={styles.subTitle}>
         simply enter your text below, and we'll let you know if you might be about to do something you'll regret ;)
         </Typography>
